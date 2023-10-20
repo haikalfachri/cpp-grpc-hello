e70e37751218f9cb9a2a0885410575f5007011ad
Muhamad Fachri Haikal <haikal.fachri18@gmail.com>
2023-10-20 23:54:08 +0700

    chore(app): initial commit

---
+++ src/client.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+#include "helloworld.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using helloworld::Greeter;
+using helloworld::HelloRequest;
+using helloworld::HelloReply;
+
+class GreeterClient {
+public:
+    GreeterClient(std::shared_ptr<Channel> channel)
+        : stub_(Greeter::NewStub(channel)) {}
+
+    std::string SayHello(const std::string& name) {
+        HelloRequest request;
+        HelloReply reply;
+        ClientContext context;
+
+        request.set_name(name);
+
+        Status status = stub_->SayHello(&context, request, &reply);
+
+        if (status.ok()) {
+            return reply.message();
+        } else {
+            return "RPC failed";
+        }
+    }
+
+private:
+    std::unique_ptr<Greeter::Stub> stub_;
+};
+
+int main() {
+    std::string server_address("localhost:50051");
+    GreeterClient greeter(grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()));
+
+    std::string user("World");
+    std::string response = greeter.SayHello(user);
+    std::cout << "Greeting: " << response << std::endl;
+
+    return 0;
+}
