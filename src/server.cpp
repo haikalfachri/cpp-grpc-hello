d5497825c50dc1f3e2e999aab520479e3710f26e
Muhamad Fachri Haikal <haikal.fachri18@gmail.com>
2023-10-25 09:05:09 +0700

    chore(app): remove unwanted code
    Co-authored-by: M Naufal Rifqi Ramdhani <mnaufalrifqir@users.noreply.github.com>

--- src/server.cpp
+++ src/server.cpp
@@ -1,41 +1,3 @@
-// #include <iostream>
-// #include <string>
-// #include <boost/asio.hpp>
-
-// using namespace boost::asio;
-// using ip::tcp;
-
-// int main()
-// {
-//     io_service io;
-//     tcp::acceptor acceptor(io, tcp::endpoint(tcp::v4(), 8080));
-//     std::cout << "Server is running on http://localhost:8080" << std::endl;
-//     while (true)
-//     {
-//         tcp::socket socket(io);
-//         acceptor.accept(socket);
-
-//         std::string response = "HTTP/1.1 200 OK\r\n";
-//         response += "Content-Type: text/event-stream\r\n";
-//         response += "Cache-Control: no-cache\r\n";
-//         response += "Connection: keep-alive\r\n";
-//         response += "\r\n";
-
-//         socket.write_some(buffer(response));
-
-//         while (true)
-//         {
-//             // Kirim peristiwa SSE ke klien
-//             std::string event = "data: Pesan SSE\r\n\r\n";
-//             std::cout << event << std::endl;
-//             socket.write_some(buffer(event));
-//             std::this_thread::sleep_for(std::chrono::seconds(3));
-//         }
-//     }
-
-//     return 0;
-// }
-
 #include <iostream>
 #include <memory>
 #include <string>
@@ -159,13 +121,11 @@ void RunServer()
         std::cout << "Server listening on " << server_address << std::endl;
 
         std::thread publisherThread(RunPublisher);
-        // std::thread subscriberThread(RunSubscriber);
         std::thread sseServerThread(RunSSESubscriber); // Add SSE server thread
 
         server->Wait();
         publisherThread.join();
-        // subscriberThread.join();
-        sseServerThread.join(); // Wait for SSE server thread to finish
+        sseServerThread.join(); 
     }
     catch (const std::exception &e)
     {
